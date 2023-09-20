7f24abe5be2dde7345833e365405ba89d0b24408
Mercy Cherono <cheronom142@gmail.com>
2023-09-20 19:47:20 +0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+// Function expression 
+const divide = function () {
+  return 2000 / 100;
+};
+
+//Arrow function with one parameter
+const square = (x) => x ** 2;
+let x = 2;
+
+//Arrow function with two parameters
+const add = (a, b) => a + b;
+let a = 3;
+let b = 4;
